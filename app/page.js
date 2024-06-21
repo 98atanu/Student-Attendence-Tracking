39c4bb0a03a00e2832e431f5c3b3f48bc5b77a01
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-06-21 23:58:44 +0530

    Authentication made.

--- app/page.js
+++ app/page.js
@@ -1,13 +1,20 @@
+'use client'
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  
+  
+  useEffect(()=> {
+    redirect('/api/auth/login?post_login_redirect_url=/dashboard')
+   
+  }, [])
   return (
-    <main className="">
-      <h1>Hiii</h1>
-      <Button>Subscribe</Button>
-      
-      
-    </main>
+    <div>
+
+    </div>
   );
 }
