6c6f2fdc17ea73a5df328d24370eb0f5ab36e691
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-07-02 23:38:42 +0530

    header created

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='p-4 shadow-sm border flex justify-between'>
+        <div>
+
+        </div>
+        <div className='w-10 p-5 bg-gray-500 rounded-full'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
